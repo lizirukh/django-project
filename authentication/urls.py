8812fdc823f35cc010f3e1892f6183e911d9bf1e
lizirukh <lizirukh@gmail.com>
2025-03-13 13:19:50 +0400

    reset password

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('change-password/', views.change_password, name='change_password'),
+    path('password_reset/', views.reset_password, name='password_reset'),
+    path('password_reset_confirm/<uidb64>/<token>/', views.reset_password_confirm, name='password-reset-confirm'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
-from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.http import HttpResponse
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+
 
 def register_user(request):
     if request.method == 'POST':
@@ -60,3 +65,42 @@ def change_password(request):
     else:
         form = PasswordChangeForm(user=request.user)
         return render(request, 'registration/change_password.html', {'form': form})
+
+def reset_password(request):
+    if request.method == 'POST':
+        form = PasswordResetForm(request.POST)
+
+        if form.is_valid:
+            form.save(
+                request=request,
+                use_https=False,
+                email_template_name='registration/password_reset_email.html',
+            )
+
+            return HttpResponse('<h2> Reset email has been successfully sent, Please check your email to finish the process. </h2')
+
+    else:
+        form = PasswordResetForm()
+        return render(request, 'registration/password_reset_request.html', {'form': form})
+
+def reset_password_confirm(request, uidb64, token):
+    try:
+        id = urlsafe_base64_decode(uidb64).decode()
+        user = User.objects.get(id=id)
+
+        if default_token_generator.check_token(user, token):
+            if request.method =='POST':
+                form = SetPasswordForm(user=user, data=request.POST)
+                if form.is_valid():
+                    form.save()
+
+                    return redirect('login')
+            else:
+                form = SetPasswordForm(user=user)
+        else:
+            return HttpResponse('<h2> Password reset token is invalid </h2>')
+
+    except (User.DoesNotExist, ValueError):
+        return redirect('password_reset')
+
+    return render(request, 'password_reset_confirm.html', {'form': form})
